64c2fd28504e77baa999ce8bec2f28dd61b40a45
mrochedy <106345479+mrochedy@users.noreply.github.com>
2024-10-12 17:13:32 +0200

    Norm mlx_clear_window.c

--- mlx_clear_window.c
+++ mlx_clear_window.c
@@ -1,21 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_clear_window.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrochedy <mrochedy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/12 17:12:48 by mrochedy          #+#    #+#             */
+/*   Updated: 2024/10/12 17:13:10 by mrochedy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 /*
-** mlx_clear_window.c for MiniLibX in 
-** 
+** mlx_clear_window.c for MiniLibX in
+**
 ** Made by Charlie Root
 ** Login   <ol@epitech.net>
-** 
+**
 ** Started on  Thu Sep  7 19:46:15 2000 Charlie Root
 ** Last update Tue Sep 25 17:11:19 2001 Charlie Root
 */
 
+#include "mlx_int.h"
 
-
-#include	"mlx_int.h"
-
-
-int		mlx_clear_window(t_xvar *xvar,t_win_list *win)
+int	mlx_clear_window(t_xvar *xvar, t_win_list *win)
 {
-  XClearWindow(xvar->display,win->window);
-  if (xvar->do_flush)
-    XFlush(xvar->display);
+	XClearWindow(xvar->display, win->window);
+	if (xvar->do_flush)
+		XFlush(xvar->display);
 }
