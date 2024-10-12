ff0a04f7f041649cc0ffc0b394e1be95736547a1
mrochedy <106345479+mrochedy@users.noreply.github.com>
2024-10-12 17:24:07 +0200

    Norm mlx_get_data_addr.c

--- mlx_get_data_addr.c
+++ mlx_get_data_addr.c
@@ -1,23 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_get_data_addr.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrochedy <mrochedy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/12 17:23:02 by mrochedy          #+#    #+#             */
+/*   Updated: 2024/10/12 17:23:35 by mrochedy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 /*
 ** mlx_get_data_addr.c for MiniLibX in raytraceur
-** 
+**
 ** Made by Charlie Root
 ** Login   <ol@epitech.net>
-** 
+**
 ** Started on  Mon Aug 14 15:45:57 2000 Charlie Root
 ** Last update Thu Sep 27 19:05:25 2001 Charlie Root
 */
 
+#include "mlx_int.h"
 
-
-#include	"mlx_int.h"
-
-
-char	*mlx_get_data_addr(t_img *img,int *bits_per_pixel,
-			   int *size_line,int *endian)
+char	*mlx_get_data_addr(t_img *img, int *bits_per_pixel,
+			int *size_line, int *endian)
 {
-  *bits_per_pixel = img->bpp;
-  *size_line = img->size_line;
-  *endian = img->image->byte_order;
-  return (img->data);
+	*bits_per_pixel = img->bpp;
+	*size_line = img->size_line;
+	*endian = img->image->byte_order;
+	return (img->data);
 }
