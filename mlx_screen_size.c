2dc6399f6a4c3b64860585d74af5bf9194a1e213
mrochedy <106345479+mrochedy@users.noreply.github.com>
2024-10-12 18:22:46 +0200

    Norm mlx_screen_size.c

--- mlx_screen_size.c
+++ mlx_screen_size.c
@@ -1,13 +1,25 @@
-#include	"mlx_int.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_screen_size.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrochedy <mrochedy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/12 18:21:21 by mrochedy          #+#    #+#             */
+/*   Updated: 2024/10/12 18:22:15 by mrochedy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int		mlx_get_screen_size(void *mlx_ptr, int *sizex, int *sizey)
+#include "mlx_int.h"
+
+int	mlx_get_screen_size(void *mlx_ptr, int *sizex, int *sizey)
 {
-	XWindowAttributes	xwAttr;
+	XWindowAttributes	xw_attr;
 	Status				ret;
 	t_xvar				*xvar;
-	
+
 	xvar = mlx_ptr;
-	ret = XGetWindowAttributes(xvar->display, xvar->root, &xwAttr);
-	(*sizex) = xwAttr.width;
-	(*sizey) = xwAttr.height;
+	ret = XGetWindowAttributes(xvar->display, xvar->root, &xw_attr);
+	(*sizex) = xw_attr.width;
+	(*sizey) = xw_attr.height;
 }
