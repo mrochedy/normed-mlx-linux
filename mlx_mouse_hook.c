ad13afdd5511e3309ff299f1f054189008d6dc0f
mrochedy <106345479+mrochedy@users.noreply.github.com>
2024-10-12 18:02:41 +0200

    Norm mlx_mouse_hook.c

--- mlx_mouse_hook.c
+++ mlx_mouse_hook.c
@@ -1,22 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_mouse_hook.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrochedy <mrochedy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/12 18:02:04 by mrochedy          #+#    #+#             */
+/*   Updated: 2024/10/12 18:02:17 by mrochedy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 /*
-** mlx_mouse_hook.c for MiniLibX in 
-** 
+** mlx_mouse_hook.c for MiniLibX in
+**
 ** Made by Charlie Root
 ** Login   <ol@epitech.net>
-** 
+**
 ** Started on  Thu Aug  3 11:49:06 2000 Charlie Root
 ** Last update Fri Feb 23 17:11:05 2001 Charlie Root
 */
 
+#include "mlx_int.h"
 
-#include	"mlx_int.h"
-
-
-
-
-int		mlx_mouse_hook(t_win_list *win,int (*funct)(),void *param)
+int	mlx_mouse_hook(t_win_list *win, int (*funct)(), void *param)
 {
-  win->hooks[ButtonPress].hook = funct;
-  win->hooks[ButtonPress].param = param;
-  win->hooks[ButtonPress].mask = ButtonPressMask;
+	win->hooks[ButtonPress].hook = funct;
+	win->hooks[ButtonPress].param = param;
+	win->hooks[ButtonPress].mask = ButtonPressMask;
 }
