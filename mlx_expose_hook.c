8d850c7a40e28a4a518e4910cf9e4b527bb92eb4
mrochedy <106345479+mrochedy@users.noreply.github.com>
2024-10-12 17:18:14 +0200

    Norm mlx_expose_hook.c

--- mlx_expose_hook.c
+++ mlx_expose_hook.c
@@ -1,22 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_expose_hook.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrochedy <mrochedy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/12 17:17:34 by mrochedy          #+#    #+#             */
+/*   Updated: 2024/10/12 17:17:53 by mrochedy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 /*
-** mlx_expose_hook.c for MiniLibX in 
-** 
+** mlx_expose_hook.c for MiniLibX in
+**
 ** Made by Charlie Root
 ** Login   <ol@epitech.net>
-** 
+**
 ** Started on  Thu Aug  3 11:49:06 2000 Charlie Root
 ** Last update Fri Feb 23 17:07:42 2001 Charlie Root
 */
 
+#include "mlx_int.h"
 
-#include	"mlx_int.h"
-
-
-
-
-int		mlx_expose_hook(t_win_list *win,int (*funct)(),void *param)
+int	mlx_expose_hook(t_win_list *win, int (*funct)(), void *param)
 {
-  win->hooks[Expose].hook = funct;
-  win->hooks[Expose].param = param;
-  win->hooks[Expose].mask = ExposureMask;
+	win->hooks[Expose].hook = funct;
+	win->hooks[Expose].param = param;
+	win->hooks[Expose].mask = ExposureMask;
 }
